bfc40a660fe4250a02e4d8ea7f706b147561bd37
Rahul Dutta <rdtech2002@gmail.com>
2023-02-07 17:08:46 +0530

    .

---
+++ first.js
@@ -0,0 +1,6 @@
+// Radious of a Circle
+
+let radius = 5;
+const pi = 3.14;
+
+console.log("Radious of the circle is : "+ (pi*radius*radius));
---
+++ p02.js
@@ -0,0 +1,13 @@
+let num = 0xff; // hex number use 0x
+console.log(num);
+let num1 = 1.5e12; // exponential
+console.log(num1);
+let num2 = 5/0; // positive infinity
+console.log(num2);
+let num3 = -5/0; // negetive infinity
+console.log(num3);
+console.log(Number.MAX_VALUE); // max number value 
+console.log(Number.MIN_VALUE); // min number value
+console.log(Number.MAX_VALUE*10); // infinity
+let num4 = 54684894198484984684849684981684698684n;
+console.log(num4); // 
